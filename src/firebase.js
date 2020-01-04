7cccce5b0bc720b3352e1813c8260e76be655d56
Black <blackng10.5.1997@gmail.com>
2020-01-04 18:10:20 +0700

    finish matches

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+var config = {
+   apiKey: "AIzaSyB66AxG7f8dtlVergc7mmj5hxYiNmJ1fTI",
+   authDomain: "football-manager-f670d.firebaseapp.com",
+   databaseURL: "https://football-manager-f670d.firebaseio.com",
+   projectId: "football-manager-f670d",
+   storageBucket: "football-manager-f670d.appspot.com",
+   messagingSenderId: "321386654070",
+   appId: "1:321386654070:web:5cbf55c912b13f60ccb257",
+   measurementId: "G-SXJPWT777E"
+ };
+
+firebase.initializeApp(config);
+//firebase.analytics();
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref('matches');
+
+// firebaseDB.ref('matches').once('value').then((snapshot) => {
+//   console.log(snapshot.val());
+// });
+
+export {
+  firebase,
+  firebaseMatches
+}
