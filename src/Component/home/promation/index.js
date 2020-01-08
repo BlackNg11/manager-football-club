99a77e7fbb7bd1e145e8c846cdc80f41c3fe8f2b
Black <blackng10.5.1997@gmail.com>
2020-01-08 18:19:44 +0700

    validData

--- src/Component/home/promation/index.js
+++ src/Component/home/promation/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import PromationAnimation from './Animation';
+import Enroll from './Enroll';
 
 const Promation = () => (
   <div className = "promotion_wrapper" style={{background: '#fff'}}>
     <div className="container">
       <PromationAnimation />
+      <Enroll />
     </div>
   </div>
 );
